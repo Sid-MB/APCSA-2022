168cd7632b92e7bc3007bbd77f0ef3f01872af33
Siddharth <64648985+Sid-MB@users.noreply.github.com>
2022-05-13 14:25:07 -0700

    actually working!! Generate spreadsheet next??

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ textArea.addEventListener("paste", paste);
 
 function submit() {
 	let gradingPeriodName = output.querySelector(".td-content-wrapper > .title").childNodes[0].textContent;
-	console.log(gradingPeriodName);
+	// console.log(gradingPeriodName);
 
 	// let categoryNames = output.querySelectorAll()
 
@@ -16,9 +16,10 @@ function submit() {
 
 	let allLines = Array.from(output.querySelectorAll(".report-row"));
 	let allTitles = allLines.map((e) => e.querySelector(".title").childNodes[0]);
-	console.log(allTitles);
+	// console.log(allTitles);
 
-	let gradebook = [
+	let gradebook = []
+		/*[
 		{
 			sectionName: "",
 			weight: 0,
@@ -30,20 +31,43 @@ function submit() {
 				},
 			],
 		},
-	];
+	];*/
 
 	//allTitles.shift()
-	allTitles = allTitles.filter((t) => t.textContent !== "(no grading period)" && t.textContent !== gradingPeriodName);
+	// allTitles = allTitles.filter((t) => t.textContent !== "(no grading period)" && t.textContent !== gradingPeriodName);
+
+	const gradeColumn = Array.from(output.querySelectorAll(".grade-column > .td-content-wrapper > .awarded-grade")).map((e) => e.textContent);
+	const sectionPercents = gradeColumn.filter((e) => e.includes("%"));
+	const awardedGrades = gradeColumn.filter((e) => !e.includes("%"));
+
+	const outOfGrades = Array.from(output.querySelectorAll(".grade-column > .td-content-wrapper > .max-grade")).map((e) => e.textContent.slice(2));
+
+	//console.log(awardedGrades)
+	//console.log(outOfGrades)
+
+	const sectionWeights = Array.from(output.querySelectorAll(".percentage-contrib")).map((e) => e.textContent.slice(1, e.textContent.length - 2));
+	// Array.from(output.querySelectorAll("reportSpacer-2 > td-content-wrapper > percentage-contrib")).map((e) => e.textContent.slice(1, e.textContent.length - 2));
+	// console.log(sectionWeights);
+
+	let assignIndex = 0;
+	let sectionIndex = 0;
 	allTitles.forEach((e) => {
-		console.log(e)
+		// console.log(e);
 		if (e.childNodes.length == 0) {
-			gradebook.push({sectionName: e.textContent, assignments: []})
+			gradebook.push({ sectionName: e.textContent, weight: sectionWeights[sectionIndex], assignments: [] });
+			sectionIndex++;
 		} else {
-			gradebook[gradebook.length - 1].assignments.push(e.childNodes[0].textContent);
+			const assignment = {
+				title: e.childNodes[0].textContent,
+				score: awardedGrades[assignIndex],
+				outOf: outOfGrades[assignIndex],
+			};
+			gradebook[gradebook.length - 1].assignments.push(assignment);
+			assignIndex++;
 		}
 	});
 
-	console.log(gradebook)
+	console.log(gradebook);
 }
 
 function paste(event) {
@@ -54,3 +78,6 @@ function paste(event) {
 
 	submit();
 }
+
+// DEBUG
+// debugOutput()
