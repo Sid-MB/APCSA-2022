6f6d53e3e20a3cc9f344555b88331ff7314e7de1
Siddharth <siddharth.m.bhatia@gmail.com>
2022-05-14 22:00:53 -0700

    Wow! Looking really good!

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function submit() {
 	//console.log(awardedGrades)
 	//console.log(outOfGrades)
 
-	const sectionWeights = Array.from(output.querySelectorAll(".percentage-contrib")).map((e) => e.textContent.slice(1, e.textContent.length - 1));
+	const sectionWeights = Array.from(output.querySelectorAll(".percentage-contrib")).map((e) => e.textContent.slice(1, e.textContent.length - 2));
 	// Array.from(output.querySelectorAll("reportSpacer-2 > td-content-wrapper > percentage-contrib")).map((e) => e.textContent.slice(1, e.textContent.length - 2));
 	// console.log(sectionWeights);
 
@@ -54,35 +54,56 @@ function submit() {
 	allTitles.forEach((e) => {
 		// console.log(e);
 		if (e.childNodes.length == 0) {
-			gradebook.push({ name: e.textContent, weight: sectionWeights[sectionIndex], assignments: [] });
+			gradebook.push({ name: e.textContent, weight: Number.parseFloat(sectionWeights[sectionIndex])/100, assignments: [] });
 			sectionIndex++;
 		} else {
 			const assignment = {
 				title: e.childNodes[0].textContent,
-				score: (awardedGrades[assignIndex] == undefined ? "-" : awardedGrades[assignIndex]),
-				outOf: (outOfGrades[assignIndex] == undefined ? "-" : outOfGrades[assignIndex]),
+				score: (awardedGrades[assignIndex] == undefined ? "-" : Number.parseFloat(awardedGrades[assignIndex])),
+				outOf: (outOfGrades[assignIndex] == undefined ? "-" : Number.parseFloat(outOfGrades[assignIndex])),
 			};
 			gradebook[gradebook.length - 1].assignments.push(assignment);
 			assignIndex++;
 		}
 	});
 
-	gradebook = gradebook.filter((section) => (section.weight != "0%" && section.name !== gradingPeriodName))
+	gradebook = gradebook.filter((section) => (section.weight != 0 && section.name !== gradingPeriodName))
 
 	console.log(gradebook);
 	generateSpreadsheet(gradebook)
 }
 
 function generateSpreadsheet(gradebook) {
-	const headerRow = document.createElement("tr");
-	const headerTitles = ["Section", "Weight", "Assignment", "Grade", "Maximum"];
+	const headerTitles = ["Section", "Weight", "Assignment", "Grade", "Maximum", null, "Course Grade"];
 
 	const bodyRows = [headerTitles];
 	gradebook.forEach((section) => {
-		let sectionInfo = [section.name, section.weight];
+		let sectionInfo = [section.name, {t: "n", z: "0%", v: section.weight}];
 		let assignmentRows = section.assignments.map((a) => [null, null, a.title, a.score, a.outOf])
-		bodyRows.push(sectionInfo, ...assignmentRows, []);
+
+		// Totaling function
+		const gradeCol = headerTitles.indexOf("Grade");
+		const maxCol = headerTitles.indexOf("Maximum");
+
+		const startRow = bodyRows.length + 1
+		const endRow = startRow + assignmentRows.length - 1;
+
+		const gradeRange = { s: { r: startRow, c: gradeCol }, e: { r: endRow, c: gradeCol } };
+		const maxRange = { s: { r: startRow, c: maxCol }, e: { r: endRow, c: maxCol } };
+
+		const gradeRangeEncoded = XLSX.utils.encode_range(gradeRange); // Encoding converts from numbers to A1 notation
+		const maxRangeEncoded = XLSX.utils.encode_range(maxRange);
+
+		const func = { t: "n", z: "0.00%", f: `IFERROR(SUM(${gradeRangeEncoded})/SUM(${maxRangeEncoded}), "-")`, D: true };
+		let totalRow = [null, null, "TOTAL", func]
+		
+
+		bodyRows.push(sectionInfo, ...assignmentRows, totalRow, []);
 	});
+
+	// Course grade calc
+	const totalGradeFunc = { t: "n", z: "0.00%", f: `SUMPRODUCT(IF(ISNUMBER(_xlfn.FILTER(D:D, C:C="TOTAL")), _xlfn.FILTER(B:B,ISNUMBER(B:B)), 0)/SUM(IF(ISNUMBER(_xlfn.FILTER(D:D, C:C="TOTAL")), _xlfn.FILTER(B:B,ISNUMBER(B:B)), 0)), _xlfn.FILTER(D:D, C:C="TOTAL"))`, D: false };
+	bodyRows[0].push(totalGradeFunc)
 	
 	const worksheet = XLSX.utils.aoa_to_sheet(bodyRows);
 	const workbook = XLSX.utils.book_new()
